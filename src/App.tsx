df151a3cccb2c2df0529645e8d505943c45d3d81
Fernando <nandokferrari@gmail.com>
2022-11-24 10:25:56 -0300

    refactor app.tsx removed unused state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="App">
             <h1>Hello woovi, let's dale</h1>
