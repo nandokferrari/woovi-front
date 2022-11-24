07ea936b5ef315f4fc521440b8bc7ff237f93841
Fernando <nandokferrari@gmail.com>
2022-11-24 14:23:50 -0300

    improvement on theme default settings

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,39 @@
 import { createTheme } from '@mui/material';
 import { red } from '@mui/material/colors';
 
+export const gridUnit = 8;
 export const WooviTheme = createTheme({
+    typography: {
+        fontFamily: 'Nunito',
+    },
     palette: {
-        primary: red,
+        primary: {
+            main: '#03D69D',
+        },
+        secondary: {
+            main: '#133A6F',
+        },
+    },
+    shape: {
+        borderRadius: 8,
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    fontSize: '18px',
+                    fontWeight: '400',
+                },
+            },
+        },
+        MuiTextField: {
+            defaultProps: {
+                variant: 'outlined',
+                fullWidth: true,
+            },
+            styleOverrides: {
+                root: {},
+            },
+        },
     },
 });
