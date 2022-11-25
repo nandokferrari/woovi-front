83bc61c162c00b0a2923cb90584a8b764a7b4438
Fernando <nandokferrari@gmail.com>
2022-11-25 11:15:46 -0300

    feature page title component

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,18 @@
+import { Typography } from '@mui/material';
+import * as React from 'react';
+import styled from 'styled-components';
+
+interface ITitleProps {
+    value: string;
+}
+
+const Container = styled(Typography)`
+    color: #4d4d4d;
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 30px;
+`;
+
+export const Title: React.FC<ITitleProps> = ({ value }) => {
+    return <Container>{value}</Container>;
+};
