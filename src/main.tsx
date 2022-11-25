6f47c6576a287adfa07c6457f3b7700649db6ded
Fernando <nandokferrari@gmail.com>
2022-11-25 11:15:08 -0300

    improvement css injection order

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { ThemeProvider } from '@mui/material';
+import { StyledEngineProvider, ThemeProvider } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -7,7 +7,9 @@ import { WooviTheme } from './theme';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider theme={WooviTheme}>
-            <App />
+            <StyledEngineProvider injectFirst>
+                <App />
+            </StyledEngineProvider>
         </ThemeProvider>
     </React.StrictMode>
 );
