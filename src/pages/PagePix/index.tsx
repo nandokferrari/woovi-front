aafbb42a3492a66dbe4bc8b6b3d23a45b90d5a92
Fernando <nandokferrari@gmail.com>
2022-11-25 14:12:49 -0300

    improvements on page pix - added QrCodeRender - added Expiration - added Summary - added Identifier

--- src/pages/PagePix/index.tsx
+++ src/pages/PagePix/index.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import { QrCodeRender } from '../../components/QrCodeRender';
+import { Expiration } from '../../components/Expiration';
+import { Identifier } from '../../components/Identifier';
+import { Summary } from '../../components/Summary';
+import { useParams } from 'react-router-dom';
 
 interface IPagePixProps {}
 
-const Container = styled.div``;
+const Container = styled.div`
+    text-align: center;
+`;
 
 export const PagePix: React.FC<IPagePixProps> = () => {
-    return <Container>pix</Container>;
+    const { uuidTransaction } = useParams();
+
+    return (
+        <Container>
+            <QrCodeRender />
+            <Expiration value={'15/12/2022 - 08:17'} />
+            <Summary />
+            <Identifier value={uuidTransaction} />
+        </Container>
+    );
 };
