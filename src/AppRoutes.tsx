3672d240cd2381028638de0e715aab3e3ef6bf06
Fernando <nandokferrari@gmail.com>
2022-11-26 18:39:45 -0300

    improvement scroll to top on navigation

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { PageCard } from './pages/PageCard';
 import { PageOptions } from './pages/PageOptions';
 import { PagePix } from './pages/PagePix';
 
-export const AppRoutes: React.FC = () => (
-    <BrowserRouter>
-        <Routes>
-            <Route index path="/" element={<PageOptions />} />
-            <Route path="/pix/:uuidTransaction" element={<PagePix />} />
-            <Route path="/card/:uuidTransaction" element={<PageCard />} />
-        </Routes>
-    </BrowserRouter>
-);
+export const AppRoutes: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <Routes>
+                <Route index path="/" element={<PageOptions />} />
+                <Route path="/pix/:uuidTransaction" element={<PagePix />} />
+                <Route path="/card/:uuidTransaction" element={<PageCard />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+const ScrollToTop = () => {
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+};
