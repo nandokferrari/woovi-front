721de27a45777103fc2814dcb175ec72ca1db833
Fernando <nandokferrari@gmail.com>
2022-11-26 21:09:12 -0300

    bug flag responsive text corrected

--- src/components/OptionsList/OptionItem/Flag/styled.tsx
+++ src/components/OptionsList/OptionItem/Flag/styled.tsx
@@ -13,14 +13,17 @@ export const Container = styled.div`
         content: '';
         display: block;
         background: white;
-        width: 22px;
-        height: 22px;
+        width: 20px;
+        height: 20px;
+        transform: translateY(-50%);
+
         position: absolute;
-        top: -3px;
-        right: -2px;
-        rotate: 45deg;
-        transform: translateX(+50%);
+        top: calc(50% - 3px);
+        right: -5px;
         border-radius: 0 0 0 3px;
+
+        rotate: 45deg;
+        /* border: 1px solid red; */
     }
 `;
 
@@ -28,8 +31,24 @@ export const Title = styled(Typography)`
     color: white;
     font-weight: 600;
     margin-right: 5px;
+
+    @media (max-width: 450px) {
+        font-size: 14px;
+    }
+
+    @media (max-width: 415px) {
+        font-size: 12px;
+    }
 `;
 
 export const Message = styled(Typography)`
     color: white;
+
+    @media (max-width: 450px) {
+        font-size: 14px;
+    }
+
+    @media (max-width: 415px) {
+        font-size: 12px;
+    }
 `;
