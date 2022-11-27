4b32c8145c48a3c4c6c4546aa822c2d9854ad6c5
Fernando <nandokferrari@gmail.com>
2022-11-27 01:21:38 -0300

    refactor relocation of options data arrays

--- src/pages/PageOptions/index.tsx
+++ src/pages/PageOptions/index.tsx
@@ -6,6 +6,7 @@ import { OptionContext } from '../../providers/OptionContext';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Loader } from '../../components/Loader';
+import { OptionsPix, OptionsPixSplitted } from '../../providers/OptionsData';
 
 interface IPageOptionsProps {}
 
@@ -18,50 +19,6 @@ const OptionsWrapper = styled.div`
     flex-direction: column;
     gap: 34px;
 `;
-
-const OptionsPix = [
-    {
-        installments: 1,
-        amount: 30500,
-        total: null,
-        cashback: 0.03,
-        banner: {
-            title: '🤑 R$ 300,00',
-            message: 'de volta no seu Pix na hora',
-        },
-    },
-];
-const OptionsPixSplitted = [
-    {
-        installments: 2,
-        amount: 15300,
-        total: 30600,
-    },
-    {
-        installments: 3,
-        amount: 10196.66,
-        total: 30620,
-    },
-    {
-        installments: 4,
-        amount: 7725,
-        total: 30900,
-        banner: {
-            title: '-3% de juros:',
-            message: 'Melhor opção de parcelamento',
-        },
-    },
-    {
-        installments: 5,
-        amount: 6300,
-        total: 31500,
-    },
-    {
-        installments: 6,
-        amount: 5283.33,
-        total: 31699.98,
-    },
-];
 
 export const PageOptions: React.FC<IPageOptionsProps> = () => {
     const { selectedItem } = React.useContext(OptionContext);
