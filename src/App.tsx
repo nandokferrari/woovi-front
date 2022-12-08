b8d9f36158b6d8a25f3d347564ae2e073137a7ca
Fernando <nandokferrari@gmail.com>
2022-12-07 22:09:42 -0300

    refactor: removed unused component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { styled, TextField, TextFieldProps } from '@mui/material';
 import { AppRoutes } from './AppRoutes';
 import { Layout } from './components/Layout';
-
-const CustomTextField = styled(TextField)<TextFieldProps>``;
 
 const App: React.FC = () => {
     return (
